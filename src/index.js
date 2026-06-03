bc0073695f1470ab2fa4a57a9870f3474712f5ba
sttrackstar <sonalt321@gmail.com>
2026-06-03 11:28:50 -0400

    Assignment completed

--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
-const dashboardTitle = document.getElementById('header');
-dashboardTitle.textContent = 'Product Dashboard';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const productList = document.getElementById('product-list');
-
-products.forEach(product => {
-const productContainer = document.createElement('div');
-productContainer.className = product.inStock ? 'card' : 'card outOfStock';
-
-  const productTitle = document.createElement('h3');
-  productTitle.textContent = product.name;
-
-  const productPrice = document.createElement('p');
-  productPrice.textContent = product.price;
-
-  const productAvailability = document.createElement('p');
-  productAvailability.textContent = product.inStock ? 'In Stock' : 'Out of Stock';
-
-  const productImage = document.createElement('img');
-  productImage.src = product.image;
-  productImage.alt = product.name;
-
-  const removeButton = document.createElement('button');
-  removeButton.textContent = 'Remove';
-  removeButton.addEventListener('click', () => {
-    productList.removeChild(productContainer);
-  });
-
-  productContainer.append(productTitle, productPrice, productAvailability, productImage, removeButton);
-  productList.appendChild(productContainer);
-});
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
